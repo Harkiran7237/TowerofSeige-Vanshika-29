277935cb1d220cb07599dac57883e6738540b3e9
happyVR961 <66651826+happyVR961@users.noreply.github.com>
2020-07-21 13:36:39 -0700

    Add files via upload

---
+++ hexagon.js
@@ -0,0 +1,20 @@
+class hexagon {
+    constructor(x,y,width,height){
+        var options ={
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      //this.image = loadImage("hexagon2.png");
+      World.add(world, this.body);
+      
+    }
+    display(){
+      //var pos =this.body.position;
+      rectMode(CENTER);
+      //fill("white");
+      rect(this.body.position.x, this.body.position.y, this.width, this.height );
+    }
+
+}
